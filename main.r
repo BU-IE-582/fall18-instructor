544a91b4248b5b4e21f787a260830eaf592d86a5
Unknown <mustafa.baydogan@algopoly.com>
2018-11-25 10:34:31 +0300

    additional parameters added, steps until modeling part is implemented

--- main.r
+++ main.r
@@ -1,7 +1,13 @@
-
 require(data.table)
 setwd('/home/baydogan/Courses/IE582/ProjectRepo/fall18-instructor/')
+
+testStart=as.Date('2018-11-16')
+trainStart=as.Date('2012-07-15')
+rem_miss_threshold=0.01 #parameter for removing bookmaker odds with missing ration greater than this fraction
+
 source('data_preprocessing.r')
+source('feature_extraction.r')
+
 
 matches_data_path='/home/baydogan/Courses/IE582/Fall18/Project/Data/df9b1196-e3cf-4cc7-9159-f236fe738215_matches.rds'
 odd_details_data_path='/home/baydogan/Courses/IE582/Fall18/Project/Data/df9b1196-e3cf-4cc7-9159-f236fe738215_odd_details.rds'
@@ -13,3 +19,7 @@ odd_details_raw=readRDS(odd_details_data_path)
 # preprocess matches
 
 matches=matches_data_preprocessing(matches_raw)
+odd_details=details_data_preprocessing(odd_details_raw,matches)
+
+features=extract_features.openclose(matches,odd_details,pMissThreshold=rem_miss_threshold,trainStart,testStart)
+
