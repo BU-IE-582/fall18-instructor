673b2298c869cf80da6c852ec678d8840d6c3843
Unknown <mustafa.baydogan@algopoly.com>
2018-11-26 12:33:05 +0300

    an efficient function to calculate RPS for predictions matrices is added

--- performance_metrics.r
+++ performance_metrics.r
@@ -5,9 +5,18 @@
 #' @param outcomes vector of 3, binary outcomes, should be provided with the same order as probs
 #' @export
 #' @examples
-RPS<- function(probs,outcomes){
+RPS_single<- function(probs,outcomes){
   probs = cumsum(probs)
   outcomes = cumsum(outcomes)
-  RPS = sum((probs-outcomes )^2)
+  RPS = sum((probs-outcomes )^2) / (length(probs)-1)
+  return(RPS)
+}
+
+RPS_matrix<- function(probs,outcomes){
+  probs=as.matrix(probs)
+  outcomes=as.matrix(outcomes)
+  probs=t(apply(t(probs), 2, cumsum))
+  outcomes=t(apply(t(outcomes), 2, cumsum))
+  RPS = apply((probs-outcomes)^2,1,sum) / (ncol(probs)-1)
   return(RPS)
 }
